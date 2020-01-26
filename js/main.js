1e4e560937ceedd5fb095dd45c8a4e1d2bc31bee
Casey The Great <casey@caseypitman.com>
2020-01-26 17:03:06 -0600

    functional - needs styles

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
 //App to determine if a given number is prime or composite
 
-//tester input
-let inputNumber = 22;
+const resetButton = document.getElementById("reset-button");
 
-//let inputNumber = document.getElementById("input-number").value;
-//let outputNumber = document.getElementById("output-number");
-//const submitButton = document.getElementById("submit-button");
-//const resetButton = document.getElementById("reset-button");
 
-let message = "";
-
+ let message = "";
 //Function to test if a number is prime or composite
 
 const primeTest = function(inputNumber){
-    let i=2;  
+    
+    let i=2; //counts up to input 
+  
     while (i<inputNumber){ //increments through divisors checking for divisibility
       let remainder = inputNumber%i;
       
       if (remainder===0){
-        message = "Composite";
+        message = inputNumber + " is COMPOSITE.";
         return message;
       }
       else if (remainder>0){
         i++;
       }
     }//end while loop
-    message = "Prime"
+    message = inputNumber + " is PRIME."
     return message;
 };
 
+//Calls and executes the primeTest function and then displays results on the page
 const execute = function(){
+    let inputNumber = document.getElementById("input-number").value;
     primeTest(inputNumber);
-    console.log(message);
-    
+    let outputNumber = document.getElementById("output-number");
+    outputNumber.innerText = message;
+}
+
+document.getElementById("submit-button").addEventListener("click", execute);
+  
+//Resets the form
+
+const reset = function(){
+  document.getElementById("output-number").innerText="";
+  document.getElementById("input-number").value="";
 };
 
-//sumbmitButton.addEventListener("click", execute);
-execute();
-  
-  //Resets the form
+//calls reset function
+document.getElementById("reset-button").addEventListener("click", reset);
