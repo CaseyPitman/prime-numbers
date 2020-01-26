e721b7c5d6efa1039d5c422dd19a593cd4142c6e
Casey The Great <casey@caseypitman.com>
2020-01-26 12:51:58 -0600

    prime/composite check working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //App to determine if a given number is prime or composite
 
 //tester input
-let inputNumber = 11;
+//let inputNumber = 11;
 
 /*
 let inputNumber = document.getElementById("input-number").value;
@@ -11,34 +11,26 @@ const resetButton = document.getElementById("reset-button");
 */
 
 //Function to test if a number is prime or composite
-let i = inputNumber-1;
+
 const primeTest = function(inputNumber){
-    //add functionality to test if input is actually a number.
-    
-
-    //try using booleans
-    while (i>0){
-
-        if (inputNumber%i===0){
-            console.log("composite");
-            return;
-        }
-        else if  (inputNumber%i>=1){
-            i--;}
-        else{
-            console.log("prime");
-            return;
-        }
-    }
-
-    
-
-}
-
-primeTest(inputNumber);
-
-
-//Calls the function to test if a number is prime or composite
-
-
-//Resets the form
+    let i=2;  
+    while (i<inputNumber){ //increments through divisors checking for divisibility
+      let remainder = inputNumber%i;
+      
+      if (remainder===0){
+        console.log("composite: " + "divisible by " + i);
+        return;
+      }
+      else if (remainder>0){
+        i++;
+      }
+    }//end while
+    console.log("prime");
+    return;
+  }
+  
+  primeTest(313);
+  
+  //Calls the function to test if a number is prime or composite
+  
+  //Resets the form
