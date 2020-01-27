0dbce46fca46623e02eee7f7ad68d3b0fff7e485
Casey The Great <casey@caseypitman.com>
2020-01-26 19:23:34 -0600

    mostly finished  -  need to account for 0 and 1.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 //App to determine if a given number is prime or composite
 
 const resetButton = document.getElementById("reset-button");
+let message = "";
 
-
- let message = "";
 //Function to test if a number is prime or composite
 
 const primeTest = function(inputNumber){
+    let i=2; //increments to input, checking for divisibility each time through
     
-    let i=2; //counts up to input 
-  
     while (i<inputNumber){ //increments through divisors checking for divisibility
       let remainder = inputNumber%i;
-      
       if (remainder===0){
         message = inputNumber + " is COMPOSITE.";
         return message;
       }
-      else if (remainder>0){
+      else if (remainder>0){ //check if inputNumber is divisible by next increment
         i++;
       }
     }//end while loop
+
     message = inputNumber + " is PRIME."
     return message;
 };
@@ -30,16 +28,15 @@ const execute = function(){
     let inputNumber = document.getElementById("input-number").value;
     primeTest(inputNumber);
     let outputNumber = document.getElementById("output-number");
-    outputNumber.innerText = message;
+    outputNumber.value = message;
 }
 
 document.getElementById("submit-button").addEventListener("click", execute);
   
 //Resets the form
-
 const reset = function(){
-  document.getElementById("output-number").innerText="";
-  document.getElementById("input-number").value="";
+  document.getElementById("output-number").value=" ";
+  document.getElementById("input-number").value=" ";
 };
 
 //calls reset function
