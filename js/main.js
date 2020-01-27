1deebc92027013852c7d6d087ce1c19a6b301eae
Casey The Great <casey@caseypitman.com>
2020-01-26 21:02:50 -0600

    ship

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-//App to determine if a given number is prime or composite
+//App to determine if a given integer is prime or composite
 
 const resetButton = document.getElementById("reset-button");
 let message = "";
 
-//Function to test if a number is prime or composite
+//Function to test if an integer is prime or composite
 
 const primeTest = function(inputNumber){
     let i=2; //increments to input, checking for divisibility each time through
@@ -26,11 +26,23 @@ const primeTest = function(inputNumber){
 //Calls and executes the primeTest function and then displays results on the page
 const execute = function(){
     let inputNumber = document.getElementById("input-number").value;
+    if (inputNumber<0){ //user inputs a negative number
+      alert("Negative numbers do not meet the definition of either prime or composite.\nPlease enter a positive integer.")
+    }
+    else if (inputNumber==0){ //user inputs zero
+      message="Zero is neither prime nor composite."
+    }
+    else if (inputNumber==1){ //user inputs one
+      message="One is neither prime nor composite."
+    }
+    else{ //user inputs a prime or composite integer
     primeTest(inputNumber);
+    }
     let outputNumber = document.getElementById("output-number");
     outputNumber.value = message;
 }
 
+//calls the execute function
 document.getElementById("submit-button").addEventListener("click", execute);
   
 //Resets the form
